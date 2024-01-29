adb8d2533f4b52e28af70b84120ffd1af13f6763
aktividades <63901386+aktividades@users.noreply.github.com>
2024-01-29 09:44:03 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,11 +8,18 @@ let Kd = 0
 let Velocidad = 0
 let TimeAnterior = 0
 let DeltaTime = 0
+let Respuesta = 0
 basic.forever(function () {
-    Diferencia = input.acceleration(Dimension.Y)
+    Diferencia = Objetivo - input.acceleration(Dimension.Y)
     DeltaY = Diferencia - DiferenciaAnterior
+    DiferenciaAnterior = Diferencia
     DeltaTime = input.runningTimeMicros() - TimeAnterior
     TimeAnterior = input.runningTimeMicros()
     Velocidad = DeltaY / DeltaTime
-    DiferenciaAnterior = Diferencia
+    Respuesta = Kp * 0 + Kd * 0
+    if (Respuesta >= 0) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, Respuesta)
+    } else {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, Respuesta)
+    }
 })
