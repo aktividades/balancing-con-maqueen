e35a219cdfd63b9730ad525ff2698cabf023bbdd
aktividades <63901386+aktividades@users.noreply.github.com>
2024-01-29 09:54:06 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,9 +2,9 @@ let Objetivo = 670
 let Diferencia = 0
 let DiferenciaAnterior = 0
 let DeltaY = 0
-let Kp = 0
-let Ki = 0
-let Kd = 0
+let Kp = 1
+let Ki = 1
+let Kd = 1
 let Velocidad = 0
 let TimeAnterior = 0
 let DeltaTime = 0
@@ -16,10 +16,10 @@ basic.forever(function () {
     DeltaTime = input.runningTimeMicros() - TimeAnterior
     TimeAnterior = input.runningTimeMicros()
     Velocidad = DeltaY / DeltaTime
-    Respuesta = Kp * 0 + Kd * 0
+    Respuesta = Kp * Math.map(Diferencia, -200, 200, -128, 128) + Kd * Math.map(Velocidad, -10, 10, -128, 128)
     if (Respuesta >= 0) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, Respuesta)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, Math.abs(Respuesta))
     } else {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, Respuesta)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, Math.abs(Respuesta))
     }
 })
