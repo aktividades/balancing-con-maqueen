562d08d86db30cf683d6ab394f91ba7e2d1bce98
aktividades <63901386+aktividades@users.noreply.github.com>
2024-01-29 08:56:16 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+let Objetivo = 670
+let Diferencia = 0
+let Kp = 0
+let Ki = 0
+let Kd = 0
+let Velocidad = 0
+let TimeAnterior = 0
+let DeltaTime = 0
 basic.forever(function () {
-    serial.writeString("X: " + input.acceleration(Dimension.X))
+	
 })
