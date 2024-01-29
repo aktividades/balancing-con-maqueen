10fae056eb2fa84db7d6cdc6b77f009875fc4028
aktividades <63901386+aktividades@users.noreply.github.com>
2024-01-29 11:23:13 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,26 @@
-let Objetivo = 700
+let Objetivo = 660
 let Diferencia = 0
 let DiferenciaAnterior = 0
 let DeltaY = 0
-let Kp = 3
+let Kp = 1
 let Ki = 1
-let Kd = 0.5
+let Kd = 10
 let Velocidad = 0
 let TimeAnterior = 0
 let DeltaTime = 0
 let Respuesta = 0
 basic.forever(function () {
-    Diferencia = Objetivo - input.acceleration(Dimension.Y)
+    Diferencia = input.acceleration(Dimension.Y) - Objetivo
     DeltaY = Diferencia - DiferenciaAnterior
     DiferenciaAnterior = Diferencia
     DeltaTime = input.runningTimeMicros() - TimeAnterior
     TimeAnterior = input.runningTimeMicros()
     Velocidad = 3000 * (DeltaY / DeltaTime)
-    Respuesta = Kp * Math.map(Diferencia, -200, 200, -128, 128) + Kd * Math.map(Velocidad, -50, 50, -128, 128)
-    if (Respuesta <= 0) {
+    Respuesta = Kp * Math.map(Diferencia, -100, 100, -128, 128) + Kd * Math.map(Velocidad, -20, 20, -128, 128)
+    if (Respuesta >= 0) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, Math.abs(Respuesta))
     } else {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, Math.abs(Respuesta))
     }
+    serial.writeValue("y", Respuesta)
 })
