97f2d1d99a3361461ef58f1b210934613f22f66b
aktividades <63901386+aktividades@users.noreply.github.com>
2024-01-29 09:05:51 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 let Objetivo = 670
 let Diferencia = 0
+let DiferenciaAnterior = 0
+let DeltaY = 0
 let Kp = 0
 let Ki = 0
 let Kd = 0
@@ -7,5 +9,10 @@ let Velocidad = 0
 let TimeAnterior = 0
 let DeltaTime = 0
 basic.forever(function () {
-	
+    Diferencia = input.acceleration(Dimension.Y)
+    DeltaY = Diferencia - DiferenciaAnterior
+    DeltaTime = input.runningTimeMicros() - TimeAnterior
+    TimeAnterior = input.runningTimeMicros()
+    Velocidad = DeltaY / DeltaTime
+    DiferenciaAnterior = Diferencia
 })
